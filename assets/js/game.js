0b20c68a8e50f0f05f5f7c08c5da78f387d01a38
kngurley <kngurley25@gmail.com>
2021-09-28 17:45:38 -0500

    added initial fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,34 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    // alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // subtract the value of player attack from the value of enemy health and use that resulte to update the value in the enemy health variable
+    enemyHealth = enemyHealth - playerAttack;
+    
+    // log a resulting message to the console so we know that it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // subtract the value of enemy attack from the value of player health and use that result to update the value in the player health variable
+    playerHealth = playerHealth - enemyAttack;
+
+    // log a resulting message to the console so we know that it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+};
+
+fight();
 

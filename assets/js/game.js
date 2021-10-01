7bb124b18dcf37b74f2d2e6d3f6be5d1fe2b6da0
kngurley <kngurley25@gmail.com>
2021-10-01 18:37:40 -0500

    fight-skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,12 +7,18 @@ var randomNumber = function(min, max) {
     return value;
   };
   
+  var fightOrSkip = function() {
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.);
+  }
+  
+  
   // fight function (now with parameter for enemy's object holding name, health, and attack values)
   var fight = function(enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
       // ask player if they'd like to fight or run
-      var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-  
+      var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter FIGHT or SKIP to choose.");
+
+
       // if player picks "skip" confirm and then stop the loop
       if (promptFight === "skip" || promptFight === "SKIP") {
         // confirm player wants to skip
